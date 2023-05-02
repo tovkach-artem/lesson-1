c765298d5ecbf4e0c022ed4d8423d091f2fe8350
Tyomych Tovkach <tyomych.tovkach@tayviscon.ru>
2023-05-02 18:49:56 +0300

    Initial commit

---
+++ section2/lesson1/task1/test/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+  @Test
+  public void testSolution() {
+    // put your test here
+    System.out.println("hqeiufhirueh");
+    Assert.fail("Tests not implemented for the task");
+  }
+}
